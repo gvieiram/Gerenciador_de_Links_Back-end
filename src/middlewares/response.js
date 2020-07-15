f82facc5aa3793364dee260ed76d7a8e357471d1
Gustavo Vieira Martins <gustavoafcavai@gmail.com>
2020-07-15 19:35:33 -0300

    Correção de bugs

--- src/middlewares/response.js
+++ src/middlewares/response.js
@@ -1,73 +1,74 @@
-const {getMessage} = require('../helpers/messages');
+const { getMessage } = require('../helpers/messages');
+
 
 const TYPE_JSON = 'application/json';
 const STATUS_CODE_OK = 200;
 const STATUS_CODE_BAD_REQUEST = 400;
 const STATUS_CODE_UNAUTHORIZED = 401;
-const STATUS_CODE_NOT_FOUD = 404;
+const STATUS_CODE_NOT_FOUND = 404;
 const STATUS_CODE_SERVER_ERROR = 500;
 
 //STATUS_CODE_OK
-const jsonOK = function(data, message, metadata){
+const jsonOK = function (data, message, metadata) {
   const status = STATUS_CODE_OK;
-  message = message ? message: getMessage('response.json_OK');
-  metadata = metadata ? metadata: {};
+  message = message ? message : 'Successful request.';
+  metadata = metadata ? metadata : {};
 
   this.status(status);
   this.type(TYPE_JSON);
-  return this.json({ message, data, metadata, status: status });  
+  return this.json({ message, data, metadata, status: status });
 };
 
 //STATUS_CODE_BAD_REQUEST
-const jsonBadRequest = function(data, message, metadata){
+const jsonBadRequest = function (data, message, metadata) {
   const status = STATUS_CODE_BAD_REQUEST;
-  message = message ? message: getMessage('response.json_Bad_Request');
-  metadata = metadata ? metadata: {};
+  message = message ? message : 'Bad request.';
+  metadata = metadata ? metadata : {};
 
   this.status(status);
   this.type(TYPE_JSON);
-  return this.json({ message, data, metadata, status: status });  
+  return this.json({ message, data, metadata, status: status });
 };
 
 //STATUS_CODE_UNAUTHORIZED
-const jsonUnauthorized = function(data, message, metadata){
+const jsonUnauthorized = function (data, message, metadata) {
   const status = STATUS_CODE_UNAUTHORIZED;
-  message = message ? message: getMessage('response.json_Unauthorized');
-  metadata = metadata ? metadata: {};
+  message = message ? message : 'Unauthorized.';
+  metadata = metadata ? metadata : {};
 
   this.status(status);
   this.type(TYPE_JSON);
-  return this.json({ message, data, metadata, status: status });  
+  return this.json({ message, data, metadata, status: status });
 };
 
 //STATUS_CODE_NOT_FOUD
-const jsonNotFound = function(data, message, metadata){
-  const status = STATUS_CODE_NOT_FOUD;
-  message = message ? message: getMessage('response.json_Not_Found');
-  metadata = metadata ? metadata: {};
+const jsonNotFound = function (data, message, metadata) {
+  const status = STATUS_CODE_NOT_FOUND;
+  message = message ? message : 'Not found.';
+  metadata = metadata ? metadata : {};
 
   this.status(status);
   this.type(TYPE_JSON);
-  return this.json({ message, data, metadata, status: status });  
+  return this.json({ message, data, metadata, status: status });
 };
 
 //STATUS_CODE_SERVER_ERROR
-const jsonServerError = function(data, message, metadata){
+const jsonServerError = function (data, message, metadata) {
   const status = STATUS_CODE_SERVER_ERROR;
-  message = message ? message: getMessage('response.json_Server_Error');
-  metadata = metadata ? metadata: {};
+  message = message ? message : 'Server error.';
+  metadata = metadata ? metadata : {};
 
   this.status(status);
   this.type(TYPE_JSON);
-  return this.json({ message, data, metadata, status: status });  
+  return this.json({ message, data, metadata, status: status });
 };
-
 const response = (req, res, next) => {
   res.jsonOK = jsonOK;
   res.jsonBadRequest = jsonBadRequest;
+  res.jsonUnauthorized = jsonUnauthorized;
   res.jsonNotFound = jsonNotFound;
   res.jsonServerError = jsonServerError;
-  
+
   next();
 };
 
